edfa13d9fa53330d3a7d8f01a09eb10b42627f66
adelaidekhasu <adelaidetshepiso1@gmail.com>
2024-03-26 23:38:28 +0200

    injected more weather data

--- src/index.js
+++ src/index.js
@@ -1,14 +1,51 @@
 function refreshWeather(response) {
-    let temperatureElement = document.querySelector("#temperature");
-    let cityElement = document.querySelector("#city");
-    cityElement.innerHTML = response.data.city;
-    temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(response.data.temperature.current);
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}mph`;
+  timeElement.innerHTML = formatDate(date);
 }
+
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = date.getDate();
+  let formattedDay = days[date.getDay()];
+  return `${formattedDay} ${day} ${hours}:${minutes}`;
+}
+
 function searchCity(city) {
   let apiKey = "8oaaaefd0359f2cf8tfe5b5d83828854";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(refreshWeather);
 }
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input").value;
