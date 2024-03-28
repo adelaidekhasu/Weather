a0e33fa525573ce67be705f2e669c40c4c3e9fc9
adelaidekhasu <adelaidetshepiso1@gmail.com>
2024-03-28 08:59:36 +0200

    added forecast

--- src/index.js
+++ src/index.js
@@ -54,7 +54,50 @@ function handleSearchSubmit(event) {
   let searchInput = document.querySelector("#search-form-input").value;
   searchCity(searchInput);
 }
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+function getForecast(city) {
+  let apiKey = "8oaaaefd0359f2cf8tfe5b5d83828854";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+function displayForecast(response) {
+  let forecastHtml = "";
+
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
+
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
+          </div>
+          <div class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}º</div>
+        </div>
+      </div>
+    `;
+    }
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+
 
 let searchformelement = document.querySelector("#search-form");
 searchformelement.addEventListener("submit", handleSearchSubmit);
 searchCity("Pretoria");
+displayForecast("Pretoria");
